66d30429c75c933b8b36ac13f16062904e5afb11
Hasan Opiev <hasanopiev@gmail.com>
2021-02-13 22:31:05 +0000

    Adds function that fetches active tab URL and prints it in the popup for the user to see (styled)

--- background.js
+++ background.js
@@ -7,4 +7,8 @@
 //     })
 // })
 
+chrome.tabs.query({active: true, windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+    document.getElementById("currentUrl").innerHTML = (tabs[0].url);
+});
+
 document.getElementById("cdDetection").innerHTML = "<p>No consent dialogue was detected</p>"
