b0ac0b6bf570ca0ead92f1e47e211f3d26b10735
Hasan Opiev <hasanopiev@gmail.com>
2021-02-21 21:12:18 +0000

    Added options page to extension. User risk options saved to chrome.storage

---
+++ options.js
@@ -0,0 +1,25 @@
+//saves options to chrome.storage
+function saveOptions(){
+    var location = document.getElementById('location-opt').checked;
+    var advertisement = document.getElementById('perAdv-opt').checked;
+    var scan = document.getElementById('scan-opt').checked;
+    var store = document.getElementById('store-opt').checked;
+    var identification = document.getElementById('identification-opt').checked;
+
+    chrome.storage.sync.set({
+        location: location,
+        advertisement: advertisement,
+        scan: scan,
+        store: store,
+        identification: identification
+    }, function() {
+        //lets user know that their options were saved
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved';
+        setTimeout(function(){
+            status.textContent='';
+        }, 750);
+    });
+}
+
+document.getElementById('save').addEventListener('click',saveOptions);
